852caac32e5eb2bd5ff3d9681fdbd05ab4f0ce0c
Alrawanhope <80806977+Alrawanhope@users.noreply.github.com>
2021-05-10 12:54:13 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function App() {
+    return(
+        <div style={{textAlign:"center"}}>
+            <h1>AmplifyAWS</h1>
+        </div>
+    )
+}
+export default App;
